167199c71227ef8eb0d698093cfc161922fd2c39
Freda Manu <fredamanu@Fredas-MacBook-Air.local>
2022-10-12 12:26:17 +0200

    adding Procfile

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ app.post("/", function (req, res) {
   })
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log("Server is running on port 3000.")
 })
